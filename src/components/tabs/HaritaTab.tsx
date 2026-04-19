9040fccda58c652de92ed4f9a409534cf14e1fa9
Dev-Mustafaa <mustafa.demir7@std.medipol.edu.tr>
2026-04-19 14:45:49 +0300

    Son push

--- src/components/tabs/HaritaTab.tsx
+++ src/components/tabs/HaritaTab.tsx
@@ -1,11 +1,9 @@
 import { MapView } from "@/components/MapView";
-import { N8nChat } from "@/components/N8nChat";
 
 export function HaritaTab() {
   return (
     <div className="relative w-full h-full">
       <MapView />
-      <N8nChat />
     </div>
   );
 }
